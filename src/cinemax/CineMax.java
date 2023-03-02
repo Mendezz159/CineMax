73b96c2654af69e3c5a457c51043db074202487c
HP PORTATIL <HP PORTATIL@HPPORTATIL>
2023-03-02 16:33:07 -0500

    Agregado Algoritmos de "sala mas rentable"

--- src/cinemax/CineMax.java
+++ src/cinemax/CineMax.java
@@ -11,7 +11,12 @@ package cinemax;
 public class CineMax {
     public static void main(String[] args) {
         int[][] asistencia = new int[15][7]; // Matriz de asistencia
-
+        int[] Precios = new int[7];
+        
+        for(int i = 0 ; i < Precios.length ; i++){
+            Precios[i]=5000-(i*100);
+        }
+        
         // Inicialización de la matriz con valores aleatorios
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 7; j++) {
@@ -20,13 +25,22 @@ public class CineMax {
         }
 
         // Cálculo de la cantidad total de personas que han visto cada película
-        int[] totalAsistencia = new int[15]; // Arreglo para almacenar los totales de asistencia
+        int[] totalAsistencia = new int[15]; // Arreglo para almacenar los totales de asistencia por pelicula
         for (int i = 0; i < 15; i++) {
             int total = 0;
             for (int j = 0; j < 7; j++) {
                 total += asistencia[i][j];
             }
             totalAsistencia[i] = total;
+        }
+        
+        int[] totalBoletos = new int[7]; // Arreglo para almacenar los totales de boletos por sala
+        for (int i = 0; i < 7; i++) {
+            int total = 0;
+            for (int j = 0; j < 15; j++) {
+                total += asistencia[j][i];
+            }
+            totalBoletos[i] = total;
         }
 
         // Determinación de la película más vista
@@ -48,11 +62,27 @@ public class CineMax {
                 maxAsistencia = asistencia[peliculaMasVista][i];
             }
         }
+        
+        int[] VentasPorSala = new int[7];
+        for(int i = 1; i < 7; i++){
+            VentasPorSala[i] = totalBoletos[i] * Precios[i];
+        }
+        
+        int SalaMasRentable = 0;
+        int maxVentas = VentasPorSala[0];
+        for (int i = 1; i < 7; i++) {
+            if (VentasPorSala[i] > maxVentas) {
+                SalaMasRentable = i;
+                maxVentas = VentasPorSala[i];
+            }
+        }
 
         // Impresión de resultados
         System.out.println(matrizToString(asistencia));
         System.out.println("La película más vista es la número " + (peliculaMasVista + 1));
         System.out.println("La mejor combinación sala-película, más vista, es la sala número " + (salaMasVista + 1) + " con la película número " + (peliculaMasVista + 1));
+        System.out.println("La Sala mas rentable es la numero " + (SalaMasRentable + 1));
+
     }
     
     public static String matrizToString(int[][] matriz) {
